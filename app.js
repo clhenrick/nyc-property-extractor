1b97ab84e35300d603d5f9f94ccf88decb824bd0
Chris Henrick <chrishenrick@gmail.com>
2014-11-04 17:28:05 -0500

    removed un-necessary route

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 var pg = require('pg');
 
 var routes = require('./routes/index');
-var amenities = require('./routes/amenities');
 var leafletData = require('./routes/leafletData');
 
 var app = express();
@@ -25,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/amenities', amenities);
 app.use('/leafletData', leafletData);
 
 // catch 404 and forward to error handler
